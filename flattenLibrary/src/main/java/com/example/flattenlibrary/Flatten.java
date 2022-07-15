fbd93487bb2a7f73ffa5eaa1d2c83eee034ea367
yash-shetye <yashshetye01@gmail.com>
2022-07-15 16:32:54 +0530

    flattern code

--- flattenLibrary/src/main/java/com/example/flattenlibrary/Flatten.java
+++ flattenLibrary/src/main/java/com/example/flattenlibrary/Flatten.java
@@ -3,8 +3,24 @@ package com.example.flattenlibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.pdftron.common.PDFNetException;
+import com.pdftron.pdf.PDFDoc;
+
+import java.io.File;
+
 public class Flatten {
     public static void simpleToast(Context context, String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+    public static File flattenDocument(File file){
+        PDFDoc doc = null;
+        try {
+            doc = new PDFDoc(String.valueOf(file));
+            doc.flattenAnnotations(true);
+        } catch (PDFNetException e) {
+            e.printStackTrace();
+        }
+
+        return file;
     }
 }
